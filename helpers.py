c3d732fa2b5f17d3311f03c2ec2ed6a60c62ac75
RomanSC <r.s.col988@gmail.com>
2018-05-30 12:08:03 -0400

    Working on velocity, acceleration, friction \(still\)

--- helpers.py
+++ helpers.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ helpers.py
 """
+from math import fabs
+from math import floor
 
 def times_list(li, dt):
     return [x * dt for x in li]
 
 def times_tup(li, dt):
     return tuple(x * dt for x in li)
+
+def float_gcd(a, b):
+    if a < b:
+        return float_gcd(b, a)
+    if fabs(b) < 0.0001:
+        return a
+    else:
+        return float_gcd(b, a - floor(a / b) * b)
+
